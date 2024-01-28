a5622fa2d48a64d521e8208589fad425d9e248ee
DoubleDie <94574113+DoubleDie@users.noreply.github.com>
2024-01-28 11:03:57 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const { Client, Message } = require('discord.js-selfbot-v13')
+const fs = require('fs')
+let token = 'NDU4MTA5MzM5MTkwOTUxOTUx.GC9nGo.kwYZlniFg1IyvTw8h5Q9EIkeSmQiHaZGSBdwEY';
+const client = new Client({
+	checkUpdate: false
+});
+
+client.on('ready', async () => {
+	console.log('Client is ready!')
+});
+
+
+
+
+client.on('messageCreate', async (message) => {
+	console.log('Message: ' + message.content)
+	console.log('------------------------------------------------------------------')
+	let messagedata = message.content
+	fs.writeFile('last_message.txt', messagedata, (err) => {
+		if (err) throw err;
+	})
+
+
+});
+
+client.login(token);
