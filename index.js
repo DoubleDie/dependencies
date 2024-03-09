6d8b2922889abb3fb3b76827064552ea15421859
DoubleDie <94574113+DoubleDie@users.noreply.github.com>
2024-03-10 03:17:49 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Client, Message } = require('discord.js-selfbot-v13')
 const fs = require('fs')
-let token = 'NDU4MTA5MzM5MTkwOTUxOTUx.GwCAL0.W2LLo1s09InXYDu7A1k0KVE7omIJEq2tLe7kvg';
+let token = 'NDU4MTA5MzM5MTkwOTUxOTUx.GCo4Zz.ZH94pjwUVfnkkNDfWbT5oXV4eRDu7GpQP6k3uA';
 const client = new Client({
 	checkUpdate: false
 });
