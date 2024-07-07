fa27f2300ee3fa373922235af84d1f05cbef2677
Robert Numrich <robcheck0102@gmail.com>
2024-07-08 02:06:25 +1000

    fixed py code

--- processor_vBYBIT_3.6_-_Trailing_Stop_Losses.py
+++ processor_vBYBIT_3.6_-_Trailing_Stop_Losses.py
@@ -65,7 +65,7 @@ def messageUpdate(content): #update latest message and ensure it is a new messag
 				print(data_dict)
 				if float(data_dict['stopLoss']) < float(data_dict['takeProfit1']):
 					connectAPI('rob', data_dict, rob_api_key, rob_secret)
-					connectAPI('holger', data_dict, holger_api_key, holger_secret)
+					#connectAPI('holger', data_dict, holger_api_key, holger_secret)
 				else:
 					#implement short trades
 					print('Short trades yet to be implemented')
@@ -149,174 +149,132 @@ def connectAPI(account, params, api_key, secret_key):
 	#check for positions
 	positions = len(bybitAPI.get_positions(category='linear', settleCoin='USDT')['result']['list'])
 	 
-	positions_open = positions
-	#positions_open += orders_open
-	if positions_open >= 2:
-		position_open = True 
-	else:
-		position_open = False
-
-	#get all parameters
-	buy_price = params['Buy Price']
-	buy_price = removeComma(buy_price)
-	buy_price = float(buy_price)
-
-	stoploss = params['Stop Loss']
-	stoploss = removeComma(stoploss)
-	stoploss = float(stoploss)
-
-	coin = params['Coin']
-
-	if params['TP2'] != '':
-		take_profit2 = float(removeComma(params['TP2']))
-		take_profit = float(removeComma(params['TP1']))
-
-	else:
-		take_profit2 = 'undefined'
-		take_profit = float(removeComma(params['TP1']))
-
+	if positions <= 2:
 	#get account balance
-	balance = bybitAPI.get_wallet_balance(accountType='UNIFIED', coin='USDT')
-	available = balance['result']['list'][0]['coin'][0]['availableToWithdraw']
-	in_positions = balance['result']['list'][0]['coin'][0]['totalPositionIM']
-	portfolio = float(available)	
-	risk_balance = portfolio + float(in_positions)
+		balance = bybitAPI.get_wallet_balance(accountType='UNIFIED', coin='USDT')
+		available = balance['result']['list'][0]['coin'][0]['availableToWithdraw']
+		in_positions = balance['result']['list'][0]['coin'][0]['totalPositionIM']
+		totalBalance = float(available) + float(in_positions)
 
-	print("||||||||||||||||||||||||||||||||||||||||||||")
-	print('|| Available balance: ' + str(portfolio) + ' ||')
-	print("||||||||||||||||||||||||||||||||||||||||||||")
-	
-
-	#get coin price
-	ticker = bybitAPI.get_tickers(category='linear', symbol=coin)
-	price = ticker['result']['list'][0]['lastPrice']
-	price = float(price)
-	
-
-	#get price diff
-	price_diff = abs(buy_price - price)
-	if price > buy_price:
-		stoploss += price_diff
-		take_profit += price_diff
-		buyprice = buy_price + price_diff
-	else:
-		stoploss -= price_diff
-		take_profit -= price_diff
-		buyprice = buy_price - price_diff
-
-	#calculate risk
-	ausd = ((0.10 * float(risk_balance))/(abs(float(stoploss) - float(buyprice))))*float(buyprice)
-	print("||||||||||||||||||||||||||||||||||||||||||||")
-	print('|| Purchase $' +str(ausd)+' worth of coin. ||')
-	print("||||||||||||||||||||||||||||||||||||||||||||")
-
-	fees = ausd * 0.02
-
-	#calculating required leverage
-	if positions_open == 0:
-		leverage = math.ceil((ausd + fees)/(portfolio/2))
-	if positions_open == 1:
-		leverage = math.ceil((ausd + fees)/portfolio)
-	if positions_open > 1:
-		abandon = True
-	if positions_open <= 1:
-		print("||||||||||||||||||||||||||||||||||||||||||||")
-		print('|| Requires ' + str(leverage) + 'x leverage ||')
-		print("||||||||||||||||||||||||||||||||||||||||||||")
-
-	#calculating qantity
-	if not abandon:
-		inst_info = bybitAPI.get_instruments_info(category='linear', symbol=coin)
-		qtyStep = inst_info['result']['list'][0]['lotSizeFilter']['minOrderQty']
-		minQty = float(qtyStep) * float(buyprice)
-		qty = math.floor(ausd)
-		qty = qty/buyprice
-		qty = math.floor(qty)
-		if qty < minQty:
-			abandon = True
-		maxlever = inst_info['result']['list'][0]['leverageFilter']['maxLeverage']
-		if leverage > float(maxlever):
-			abandon = True
-		price = str(price)
-		if '.' in price:
-			dec = price.split('.')[1]
-			lpo = len(dec)
-		else:
-			lpo = 0
-
-
-	if not position_open and not abandon:
-		#setting leverage
-		try:
-			bybitAPI.set_leverage(category='linear', symbol=coin, buyLeverage=str(leverage), sellLeverage=str(leverage))
-		except:
-			x = 1
-		print(lpo)
-		trailing = str(round(abs(float(buyprice) - float(take_profit)), lpo))
-		take_profit = str(round(float(take_profit), lpo))
-		stoploss = str(round(float(stoploss), lpo))
+		#get coin price
+		ticker = bybitAPI.get_tickers(category='linear', symbol=coin)
+		currentPrice = float(ticker['result']['list'][0]['lastPrice'])
 		
-		#placing trade
-		init_order = bybitAPI.place_order(
-			category='linear',
-			symbol=coin,
-			side='Buy',
-			orderType='Limit',
-			qty=str(qty),
-			price=str(buyprice)
-			)
-		print(init_order)
-		new_position = 0
-		while new_position == 0:
-			time.sleep(0.5)
-			new_position = len(bybitAPI.get_positions(category='linear', symbol=coin)['result']['list'])
-			print(new_position)
-		time.sleep(2)
-		if take_profit2 != 'undefined':
-			first_stop_order = bybitAPI.set_trading_stop(
-				category='linear',
-				symbol=coin,
-				takeProfit=str(take_profit),
-				stopLoss=str(stoploss),
-				tpslMode='Partial',
-				tpOrderType='Market',
-				slOrderType='Market',
-				slSize=str(float(qty)/2),
-				tpSize=str(float(qty)/2),
-				positionIdx=0
-				)
 
-			second_stop_order = bybitAPI.set_trading_stop(
-				category='linear',
-				symbol=coin,
-				takeProfit=str(take_profit2),
-				stopLoss=str(stoploss),
-				tpslMode='Partial',
-				tpOrderType='Market',
-				slOrderType='Market',
-				slSize=str(float(qty)/2),
-				tpSize=str(float(qty)/2),
-				trailingStop=trailing,
-				activePrice=str(take_profit),
-				positionIdx=0
-				)
+		#get price diff
+		price_diff = abs(float(params["buyPrice"]) - currentPrice)
+		if price > float(params["buyPrice"]):
+			params["stopLoss"] = str(float(params["stopLoss"]) + price_diff)
+			params["takeProfit1"] = str(float(params["takeProfit1"]) + price_diff)
+			if params["takeProfit2"] != "":
+				params["takeProfit2"] = str(float(params["takeProfit2"]) + price_diff)
+			params["buyPrice"] = str(float(params["buyPrice"]) + price_diff)
 		else:
-			stop_order = bybitAPI.set_trading_stop(
-				category='linear',
-				symbol=coin,
-				takeProfit=str(take_profit),
-				stopLoss=str(stoploss),
-				tpslMode='Partial',
-				tpOrderType='Market',
-				slOrderType='Market',
-				slSize=str(qty),
-				tpSize=str(qty),
-				trailingStop=trailing,
-				activePrice=(take_profit),
-				positionIdx=0
-				)
+			params["stopLoss"] = str(float(params["stopLoss"]) - price_diff)
+			params["takeProfit1"] = str(float(params["takeProfit1"]) - price_diff)
+			if params["takeProfit2"] != "":
+				params["takeProfit2"] = str(float(params["takeProfit2"]) - price_diff)
+			params["buyPrice"] = str(float(params["buyPrice"]) - price_diff)
 
-		orderTable(coin, buyprice, ausd, leverage, stoploss, take_profit)
+		#calculate risk
+		fiatQuantity = ((0.10 * float(totalBalance))/(abs(float(params["stopLoss"])) - float(params["buyPrice"])))*float(params["buyPrice"])
+
+		#calculating required leverage
+		if positions == 0:
+			leverage = math.ceil((fiatQuantity)/(available/2))
+		if positions == 1:
+			leverage = math.ceil((fiatQuantity)/available)
+
+		#calculating qantity
+		inst_info = bybitAPI.get_instruments_info(category='linear', symbol=params["Coin"])
+		minOrderQty = inst_info['result']['list'][0]['lotSizeFilter']['minOrderQty']
+		orderQty = float(fiatQuantity) / float(params["buyPrice"])
+		if "." in str(minOrderQty):
+			roundTo = len(str(minOrderQty).split(".")[1])
+			orderQty = round(orderQty, roundTo)
+		else:
+			math.floor(orderQty)
+		if orderQty >= minOrderQty:	
+			#checking maximum leverage is above used leverage
+			maxlever = inst_info['result']['list'][0]['leverageFilter']['maxLeverage']
+			if leverage < float(maxlever):
+				#rounding all price figures to the instruments native figures
+				if '.' in params["buyPrice"]:
+					roundTo = len(params["buyPrice"].split('.')[1])
+					
+				else:
+					roundTo = 0
+
+				#setting leverage
+				try:
+					bybitAPI.set_leverage(category='linear', symbol=params["Coin"], buyLeverage=str(leverage), sellLeverage=str(leverage))
+				except:
+					x = 1
+				trailing = str(round(abs(float(params["buyPrice"]) - float(take_profit)), roundTo))
+				
+				#placing trade
+				init_order = bybitAPI.place_order(
+					category='linear',
+					symbol=params["Coin"],
+					side='Buy',
+					orderType='Limit',
+					qty=str(orderQty),
+					price=params["buyPrice"]
+					)
+				orderId = init_order["result"]["orderId"]
+				print(init_order)
+				new_position = "0"
+				while new_position == "0" and timer < 60:
+					time.sleep(0.5)
+					new_position = len(bybitAPI.get_positions(category='linear', symbol=params["Coin"])['result']['list'][0]["avgPrice"])
+					timer += 1
+				if timer >= 60:
+					print("Trade aborted: Position not filled in time.")
+					bybitAPI.cancel_order(category="linear", symbol=params["Coin"], orderId=orderId)
+
+				if params["takeProfit2"] != "":
+					first_stop_order = bybitAPI.set_trading_stop(
+						category='linear',
+						symbol=params["Coin"],
+						takeProfit=params["takeProfit1"],
+						stopLoss=params["stopLoss"],
+						tpslMode='Partial',
+						tpOrderType='Market',
+						slOrderType='Market',
+						slSize=str(float(orderQty)/2),
+						tpSize=str(float(orderQty)/2),
+						positionIdx=0
+						)
+
+					second_stop_order = bybitAPI.set_trading_stop(
+						category='linear',
+						symbol=params["Coin"],
+						takeProfit=params["takeProfit2"],
+						stopLoss=params["stopLoss"],
+						tpslMode='Partial',
+						tpOrderType='Market',
+						slOrderType='Market',
+						slSize=str(float(orderQty)/2),
+						tpSize=str(float(orderQty)/2),
+						trailingStop=trailing,
+						activePrice=params["takeProfit1"],
+						positionIdx=0
+						)
+				else:
+					stop_order = bybitAPI.set_trading_stop(
+						category='linear',
+						symbol=params["Coin"],
+						takeProfit=params["takeProfit1"],
+						stopLoss=params["stopLoss"],
+						tpslMode='Partial',
+						tpOrderType='Market',
+						slOrderType='Market',
+						slSize=str(orderQty),
+						tpSize=str(orderQty),
+						positionIdx=0
+						)
+
+				orderTable(params["Coin"], params["buyPrice"], fiatQuantity, leverage, params["stopLoss"], params["takeProfit1"])
 
 def removeComma(number):
 	number = number.split(',')
@@ -328,7 +286,7 @@ def orderTable(name, price, size, lever, stoploss, takeprofit):
 	sizeLine = 'ORDER SIZE: ' + str(size)
 	leverLine = 'LEVERAGE: ' + str(lever)
 	stopLine = 'STOP LOSS: ' + str(stoploss)
-	profitLine = 'TAKE PROFIT: ' + str(takeprofit)
+	profitLine = 'TAKE PROFIT 1: ' + str(takeprofit)
 	top_bottom = ''
 	lines = [nameLine, priceLine, sizeLine, leverLine, stopLine, profitLine]
 	lines2 = []
